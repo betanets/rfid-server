283495077dc7d0920e3a81f86fefcd12641a3a30
Alexander Shkirkov <can3456@mail.ru>
2018-03-02 15:27:06 +0700

    Created database structure Created visitors form components

---
+++ RFIDServer/RFIDServer/VisitorsForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace RFIDServer
+{
+    partial class VisitorsForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button_access_settings = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel_dbStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Snow;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(860, 395);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button_access_settings
+            // 
+            this.button_access_settings.Location = new System.Drawing.Point(744, 413);
+            this.button_access_settings.Name = "button_access_settings";
+            this.button_access_settings.Size = new System.Drawing.Size(128, 23);
+            this.button_access_settings.TabIndex = 1;
+            this.button_access_settings.Text = "Настройка доступа";
+            this.button_access_settings.UseVisualStyleBackColor = true;
+            this.button_access_settings.Click += new System.EventHandler(this.button_access_settings_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel_dbStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 439);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(884, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel_dbStatus
+            // 
+            this.toolStripStatusLabel_dbStatus.Name = "toolStripStatusLabel_dbStatus";
+            this.toolStripStatusLabel_dbStatus.Size = new System.Drawing.Size(173, 17);
+            this.toolStripStatusLabel_dbStatus.Text = "Подключение к базе данных...";
+            // 
+            // VisitorsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button_access_settings);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "VisitorsForm";
+            this.Text = "RFID сервер";
+            this.Load += new System.EventHandler(this.VisitorsForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button_access_settings;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_dbStatus;
+    }
+}
+
---
+++ RFIDServer/RFIDServer/VisitorsForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RFIDServer
+{
+    public partial class VisitorsForm : Form
+    {
+        public VisitorsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button_access_settings_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void VisitorsForm_Load(object sender, EventArgs e)
+        {
+            //Thread dbThread = new Thread(DBThreadInitiator);
+            //dbThread.Start();
+            DBThreadInitiator();
+        }
+
+        private async void DBThreadInitiator()
+        {
+            await Task.Delay(1); //Delay for async initialization
+            SQLiteConnection conn = new SQLiteConnection("Data Source=access.db; Version=3; Foreign Keys=True;");
+            try
+            {
+                conn.Open();
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show("Произошла ошибка при подключении к базе данных: " + ex.Message);
+                toolStripStatusLabel_dbStatus.Text = "Ошибка подключения к базе данных";
+            }
+
+            if (conn.State == ConnectionState.Open)
+            {
+                toolStripStatusLabel_dbStatus.Text = "База данных доступна";
+                SQLiteCommand cardsCommand = conn.CreateCommand();
+                cardsCommand.CommandText = "CREATE TABLE IF NOT EXISTS cards(" +
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                    "card_serial TEXT, " +
+                    "owner TEXT, " +
+                    "access_status INTEGER DEFAULT 0" + //0 - forbidden, 1 - allowed
+                    ");";
+                try
+                {
+                    cardsCommand.ExecuteNonQuery();
+                }
+                catch (SQLiteException ex)
+                {
+                    MessageBox.Show("Произошла ошибка при создании таблицы cards: " + ex.Message);
+                }
+
+                SQLiteCommand visitorsCommand = conn.CreateCommand();
+                visitorsCommand.CommandText = "CREATE TABLE IF NOT EXISTS visitors(" +
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                    "card_id INTEGER NOT NULL, " +
+                    "datetime INTEGER, " + //UNIX time
+                    "CONSTRAINT fk_card_id FOREIGN KEY(card_id) REFERENCES cards(id)" + 
+                    ");";
+                try
+                {
+                    visitorsCommand.ExecuteNonQuery();
+                }
+                catch (SQLiteException ex)
+                {
+                    MessageBox.Show("Произошла ошибка при создании таблицы visitors: " + ex.Message);
+                }
+            }
+
+            conn.Dispose();
+        }
+    }
+}
